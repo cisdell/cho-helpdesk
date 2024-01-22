82cace23ad9e19154bbf78da759367ce789e0725
Andrew Cho <cisdell@gmail.com>
2024-01-22 14:59:51 -0500

    added more end points

--- app/api/tickets/route.js
+++ app/api/tickets/route.js
@@ -1,5 +1,7 @@
 import { NextResponse } from "../../../node_modules/next/server";
 //'localhost:3000/api/tickets'
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   const res = await fetch("http://localhost:4000/tickets");
 
@@ -7,5 +9,21 @@ export async function GET() {
 
   return NextResponse.json(tickets, {
     status: 200,
+  });
+}
+
+export async function POST(request) {
+  const ticket = await request.json();
+
+  const res = await fetch("http://localhost:4000/tickets", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(ticket),
+  });
+
+  const newTicket = await res.json();
+
+  return NextResponse.json(newTicket, {
+    status: 201,
   });
 }
