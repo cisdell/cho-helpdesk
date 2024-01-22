6696222b3213153d89172d84068edbf705b834a8
Andrew Cho <cisdell@gmail.com>
2024-01-22 13:05:50 -0500

    auth and signup base

---
+++ app/(auth)/login/page.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Main } from "../../../node_modules/next/document";
+
+export default function Login() {
+  return (
+    <main>
+      <h2 className="text-center"></h2>
+    </main>
+  );
+}
