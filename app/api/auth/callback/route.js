f481a56ce512185d7325671d94471c3ae2e666bf
Andrew Cho <cisdell@gmail.com>
2024-01-22 19:20:31 -0500

    auth get request done

---
+++ app/api/auth/callback/route.js
@@ -0,0 +1,15 @@
+import { createRouteHandlerClient } from "../../../../node_modules/@supabase/auth-helpers-nextjs/dist/index";
+import { cookies } from "next/headers";
+import { NextResponse } from "../../../../node_modules/next/server";
+
+export async function GET(request) {
+  const url = new URL(request.url);
+  const code = url.searchParams.get("code");
+
+  if (code) {
+    const supabase = createRouteHandlerClient({ cookies });
+    await supabase.auth.exchangeCodeForSession(code);
+  }
+
+  return NextResponse.redirect(url.origin);
+}
