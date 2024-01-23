ecc093c0a57b7860316df1ef330b82c80fd10e0b
Andrew Cho <cisdell@gmail.com>
2024-01-23 16:30:00 -0500

    auth fixed

--- app/(dashboard)/layout.jsx
+++ app/(dashboard)/layout.jsx
@@ -5,6 +5,7 @@ import { cookies } from "next/headers";
 export default async function DashboardLayout({ children }) {
   const supabase = createServerComponentClient({ cookies });
   const { data } = await supabase.auth.getSession();
+  console.log(data);
 
   return (
     <>
