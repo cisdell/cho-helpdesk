72bc81309d409cff4ba5e2d71c1ee6bcf3396526
Andrew Cho <cisdell@gmail.com>
2024-01-25 18:19:16 -0500

    deleted button

--- app/(dashboard)/tickets/[id]/DeleteButton.jsx
+++ app/(dashboard)/tickets/[id]/DeleteButton.jsx
@@ -1,13 +1,28 @@
 "use client";
 import { useState } from "react";
 import { TiDelete } from "react-icons/ti";
+import { useRouter } from "next/navigation";
 
-export default function DeleteButton() {
+export default function DeleteButton({ id }) {
   const [isLoading, setIsLoading] = useState(false);
-
+  const router = useRouter();
   const handleClick = async () => {
     setIsLoading(true);
     console.log("deleting id-", id);
+
+    const res = await fetch(`http://localhost:3000/api/tickets/${id}`, {
+      method: "DELETE",
+    });
+    const json = await res.json();
+
+    if (json.error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+    if (!json.error) {
+      router.refresh();
+      router.push("/tickets");
+    }
   };
   return (
     <button className="btn-primary" onClick={handleClick} disabled={isLoading}>
--- app/api/tickets/[id]/route.js
+++ app/api/tickets/[id]/route.js
@@ -1,18 +1,28 @@
-import { NextResponse } from "../../../../node_modules/next/server";
-//'localhost:3000/api/tickets'
-export async function GET(_, { params }) {
+import { NextResponse } from "next/server";
+import { createRouteHandlerClient } from "../../../../node_modules/@supabase/auth-helpers-nextjs/dist/index";
+import { cookies } from "../../../../node_modules/next/headers";
+export async function DELETE(_, { params }) {
   const id = params.id;
-  const res = await fetch(`http://localhost:4000/tickets/${id}`);
-  const ticket = await res.json();
-  console.log(res);
-  if (!res.ok) {
-    console.log("sdfsdfsdfsfsdf!!");
-    return NextResponse.json(
-      { error: "Cannot find the ticket" },
-      { status: 404 }
-    );
-  }
-  return NextResponse.json(ticket, {
-    status: 200,
-  });
+  const supabase = createRouteHandlerClient({ cookies });
+
+  const { error } = await supabase.from("tickets").delete().eq("id", id);
+
+  return NextResponse.json({ error });
 }
+//'localhost:3000/api/tickets'
+// export async function GET(_, { params }) {
+//   const id = params.id;
+//   const res = await fetch(`http://localhost:4000/tickets/${id}`);
+//   const ticket = await res.json();
+//   console.log(res);
+//   if (!res.ok) {
+//     console.log("sdfsdfsdfsfsdf!!");
+//     return NextResponse.json(
+//       { error: "Cannot find the ticket" },
+//       { status: 404 }
+//     );
+//   }
+//   return NextResponse.json(ticket, {
+//     status: 200,
+//   });
+// }
